2c48d4c683ac72e1bc968a28ed9213307748c379
edsuco <micaelasuarezco@gmail.com>
2024-09-28 14:24:39 -0500

    feat: Se agregó servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { AboutComponent } from './components/about/about.component';
+import { HeroesService } from './services/heroes.service';
 
 
 @NgModule({
@@ -21,7 +22,7 @@ import { AboutComponent } from './components/about/about.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [HeroesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html'
 })
 export class HeroesComponent {
+  heroes: any[] = []
+
+  constructor(private heroesService: HeroesService) {    
+    this.heroes = this.heroesService.getHeroes()
+    console.log(this.heroes);
+    
+  }
+
 
 }
