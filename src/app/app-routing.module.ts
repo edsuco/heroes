8a68bbf553236a6d4f4c5a2fa655baa35b83b621
edsuco <micaelasuarezco@gmail.com>
2024-09-28 13:46:53 -0500

    feat: Se agregó rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'heroes', component:HeroesComponent},
+  {path:'about', component:AboutComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
