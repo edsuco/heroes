b95be9484e625afd5b4d948495484aaf9b27a7a9
edsuco <micaelasuarezco@gmail.com>
2024-09-29 22:44:51 -0500

    feat: Se busca héroe con el evento click y keyup.enter

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { HeroesService } from '../../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
+  constructor(private heroesService:HeroesService, private router:Router){
+  }
 
+  buscarHeros(termino:string){   
+    // this.heroesService.buscarHeroes(termino)
+    this.router.navigate(['/busqueda', termino])
+  }
+  handleKeyupEnter(event:any) {    
+    if(event.code=="Enter")
+    event.preventDefault();
+
+  }
 }
